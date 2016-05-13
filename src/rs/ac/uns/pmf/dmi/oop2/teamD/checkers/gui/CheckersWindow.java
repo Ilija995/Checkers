3ce4e6333810d66d4be3572f263489912af2a100
prske <aleksandartomcic@yahoo.com>
2016-05-13 20:20:23 +0200

    Work?

--- src/rs/ac/uns/pmf/dmi/oop2/teamD/checkers/gui/CheckersWindow.java
+++ src/rs/ac/uns/pmf/dmi/oop2/teamD/checkers/gui/CheckersWindow.java
@@ -6,5 +6,9 @@ import javax.swing.*;
  * Our checkers game window
  * Apart from creating and maintaining gui it responds when other player sends his move
  */
+
+/**
+ * Something
+ */
 public class CheckersWindow extends JFrame {
 }
