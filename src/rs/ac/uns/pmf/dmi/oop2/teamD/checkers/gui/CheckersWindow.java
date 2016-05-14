203cf9d8455e62a64a241602137c27324b95d196
prske <aleksandartomcic@yahoo.com>
2016-05-14 02:10:29 +0200

    no message

--- src/rs/ac/uns/pmf/dmi/oop2/teamD/checkers/gui/CheckersWindow.java
+++ src/rs/ac/uns/pmf/dmi/oop2/teamD/checkers/gui/CheckersWindow.java
@@ -1,14 +1,57 @@
 package rs.ac.uns.pmf.dmi.oop2.teamD.checkers.gui;
 
 import javax.swing.*;
+import java.awt.*;
+
 
 /**
  * Our checkers game window
  * Apart from creating and maintaining gui it responds when other player sends his move
  */
 
-/**
- * Something
- */
 public class CheckersWindow extends JFrame {
+
+    private static final int NUM_BTN = 100;
+
+    private JButton[] fields;
+    private Icon bluePawn;
+    private Icon orangePawn;
+    private Icon blueQueen;
+    private Icon orangeQueen;
+
+
+    public CheckersWindow() {
+        bluePawn = new ImageIcon("res\\blue.png");
+        orangePawn = new ImageIcon("res\\orange.png");
+        blueQueen = new ImageIcon("res\\blueQ.png");
+        orangeQueen = new ImageIcon("res\\orangeQ.png");
+
+        initTable();
+
+    }
+
+    private void initTable() {
+        getContentPane().removeAll();
+        setLayout(new GridLayout(10, 10));
+
+        fields = new JButton[NUM_BTN];
+
+    }
+
+    public static void main(String[] a) {
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ex) {
+
+        }
+
+        CheckersWindow chw = new CheckersWindow();
+        chw.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        chw.setSize(500, 500);
+        chw.setTitle("Checkers");
+        chw.setVisible(true);
+    }
+
+
 }
